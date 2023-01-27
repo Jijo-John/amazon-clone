d0d226ec1ca2fdcfe7c00469c86265b8cbc36d5c
Jijo-John <jijojohny358@gmail.com>
2023-01-27 10:49:54 +0530

    Adding Header

--- src/App.js
+++ src/App.js
@@ -5,15 +5,18 @@ import './App.css';
 // /checkout
 // /login 
 import { BrowserRouter as Router, Switch, Route  } from 'react-router-dom';
+import Header from './Components/Header';
 
 function App() {
   return (
     <Router>
         <div className="app">
-          
+          <Header />
         </div>
     </Router>
   );
 } 
+
+
 
 export default App;
