8a9b22af01df19a32189539279f6cbcbcee8dbe0
Jijo-John <jijojohny358@gmail.com>
2023-02-01 11:59:26 +0530

    removing unused container

--- src/Components/FluidCard.jsx
+++ src/Components/FluidCard.jsx
@@ -1,43 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
 import styled from 'styled-components'
 
 const FluidCard = ({ head, data }) => {
     
     return (
         <Container>
-            <div className='container'>
-                <h3 className='heading'>{head}</h3>
+            <h3 className='heading'>{head}</h3>
                 <div className='img-container'>
-            {data && data.map(({title,image})=>{
+                {data && data.map(({title,image})=>{
                     return(
                         <div className='item'>
                             <img src={image} alt={title}/>
                         </div>
                     )
-            })}
-            </div>
+                })}
             </div>
         </Container>
     )
 }
 
 const Container = styled.div`
-    width: 100%;
-    .container {
-        background-color: #ffffff;
-        width: 309px;
+        overflow: hidden;
         display: flex;
         flex-direction: column;
-        align-items: center;
         justify-content: center;
+        /* border: 1px dashed; */
+        padding: 10px;
+        background-color: white;
+
+        /* flex: 1 0 21%; */
+    .img-container {
         overflow: hidden;
-        border: 1px solid;
     }
-    .container > .heading {
-        width: 100%;
-    }
-    .container > .img-container {
-        height: 300px;
+    .img-container > .item {
+        /* border: 1px dotted red; */
+        overflow: hidden;
     }
 `;
 
